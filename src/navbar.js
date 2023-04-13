7b167003ee337c4b24843813dafe07abf5142c91
PGS <sarangipriyanshu@gmail.com>
2023-04-13 17:11:52 +0530

    Changes

--- src/navbar.js
+++ src/navbar.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
     return (  
         <div className="navbar">
-            <h1>Dojo Blogs</h1>
+            <h1> Football Blogs</h1>
             <div className="links">
                 <Link to="/">Home</Link>
                 <Link to ="/add">Create A New Blog</Link>
